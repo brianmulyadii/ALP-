0323bd5405de38844f95714a587b19834e70cb6a
GokaiInfinity <patriciofilbert@gmail.com>
2023-01-08 18:59:07 +0700

    fitur simulasi

--- src/CariKalori.java
+++ src/CariKalori.java
@@ -54,7 +54,7 @@ public class CariKalori {
 
     public int pilih(int kalori) {
         int pilih;
-        System.out.println("Target kalori harian = "+ kalori);
+        System.out.println("Target kalori harian = " + kalori);
         System.out.println("Menu: ");
         System.out.println("1. Menghitung kalori harian yang dibutuhkan");
         System.out.println("2. Menghitung BMI");
@@ -67,7 +67,8 @@ public class CariKalori {
         System.out.println("9. Mengedit daftar makanan");
         System.out.println("10. Menghapus daftar makanan");
         System.out.println("11. Target Kalori");
-        System.out.println("12. Keluar");
+        System.out.println("12. Simulasi Makan dan Olahraga (Peringatan : Input Daftar Olahraga, Makanan, serta Target Kalori sebelum menggunakan fitur ini !!)");
+        System.out.println("13. Keluar");
         System.out.print("Masukkan pilihan: ");
 
         //error check pada pilihan jika tidak masuk integer
@@ -178,25 +179,20 @@ public class CariKalori {
         System.out.println("");
     }
 
-    public ArrayList<String> daftarolahraga(ArrayList<String> olahraga) {
+    public void daftarolahraga(ArrayList<String> olahraga, ArrayList<Integer> kaloriolahraga) {
 
         System.out.println("Daftar Olahraga: ");
         for (int i = 0; i < olahraga.size(); i++) {
-            System.out.println((i + 1) + ". " + olahraga.get(i));
+            System.out.println((i + 1) + ". " + olahraga.get(i) + " " + "\t" + kaloriolahraga.get(i) + " kalori");
         }
         System.out.println("");
-        return olahraga;
     }
 
-    public ArrayList<String> tambaholahraga(ArrayList<String> olahraga) {
+    public ArrayList<String> tambaholahraga(ArrayList<String> olahraga, ArrayList<Integer> kaloriolahraga) {
         String tambah, ya;
         boolean lagi;
-        int angka;
+        int angka, kalori;
         System.out.println("Tambah daftar olahraga: ");
-        System.out.println("Daftar Olahraga: ");
-        for (int i = 0; i < olahraga.size(); i++) {
-            System.out.println((i + 1) + ". " + olahraga.get(i));
-        }
         System.out.println("");
         do {
             System.out.println("1. Tambah olahraga");
@@ -217,6 +213,14 @@ public class CariKalori {
             System.out.print("Nama olahraga: ");
             tambah = scan.next() + scan.nextLine();
             olahraga.add(tambah);
+            System.out.print("Berapa Kalori: ");
+            while (!scan.hasNextInt()) {
+                System.out.println("Input tidak valid !!");
+                scan.next();
+                System.out.print("Berapa Kalori: ");
+            }
+            kalori = scan.nextInt();
+            kaloriolahraga.add(kalori);
             System.out.println("Olahraga berhasil ditambahkan !!");
             System.out.println("Apakah anda ingin menambah daftar olahraga lagi ? (y/n)");
             ya = scan.next();
@@ -230,14 +234,10 @@ public class CariKalori {
         return olahraga;
     }
 
-    public ArrayList<String> editolahraga(ArrayList<String> olahraga) {
-        int angka, pilih;
+    public ArrayList<String> editolahraga(ArrayList<String> olahraga, ArrayList<Integer> kaloriolahraga) {
+        int angka, pilih, kalori;
         String nama;
         System.out.println("Edit daftar olahraga: ");
-        System.out.println("Daftar Olahraga: ");
-        for (int i = 0; i < olahraga.size(); i++) {
-            System.out.println((i + 1) + ". " + olahraga.get(i));
-        }
         System.out.println("");
         System.out.print("Edit daftar olahraga no berapa?(pilih 0 untuk keluar): ");
         while (!scan.hasNextInt()) {
@@ -253,20 +253,28 @@ public class CariKalori {
                 System.out.print("Nama olahraga: ");
                 nama = scan.nextLine() + scan.nextLine();
                 olahraga.set(angka - 1, nama);
+                System.out.print("Berapa Kalori: ");
+                while (!scan.hasNextInt()) {
+                    System.out.println("Input tidak valid !!");
+                    scan.next();
+                    System.out.print("Berapa Kalori: ");
+                }
+                kalori = scan.nextInt();
+                kaloriolahraga.set(angka - 1, kalori);
                 System.out.println("Olahraga berhasil diedit !!");
             }
-        } 
-        else if (angka < 0){
+        } else if (angka < 0) {
             System.out.println("Angka tidak valid !!");
         }
         System.out.println("");
         return olahraga;
     }
 
-    public ArrayList<String> hapusolahraga(ArrayList<String> olahraga) {
+    public void hapusolahraga(ArrayList<String> olahraga, ArrayList<Integer> kaloriolahraga) {
         int angka, pilih;
         String nama;
         System.out.println("Hapus daftar olahraga: ");
+        System.out.println("");
         System.out.println("Daftar Olahraga: ");
         for (int i = 0; i < olahraga.size(); i++) {
             System.out.println((i + 1) + ". " + olahraga.get(i));
@@ -284,33 +292,28 @@ public class CariKalori {
                 System.out.println("Tidak ada yang bisa dihapus !!");
             } else {
                 olahraga.remove(angka - 1);
+                kaloriolahraga.remove(angka - 1);
             }
-        }
-        else if (angka < 0){
+        } else if (angka < 0) {
             System.out.println("Angka tidak valid !!");
         }
         System.out.println("");
-        return olahraga;
     }
 
-    public ArrayList<String> daftarmakan(ArrayList<String> makan) {
+    public ArrayList<String> daftarmakan(ArrayList<String> makan, ArrayList<Integer> kalorimakan) {
         System.out.println("Daftar Makan: ");
         for (int i = 0; i < makan.size(); i++) {
-            System.out.println((i + 1) + ". " + makan.get(i));
+            System.out.println((i + 1) + ". " + makan.get(i) + " " + "\t" + kalorimakan.get(i) + " kalori");
         }
         System.out.println("");
         return makan;
     }
 
-    public ArrayList<String> tambahmakan(ArrayList<String> makan) {
+    public void tambahmakan(ArrayList<String> makan, ArrayList<Integer> kalorimakan) {
         String tambah, ya;
         boolean lagi;
-        int angka;
+        int angka, kalori;
         System.out.println("Tambah daftar makanan: ");
-        System.out.println("Daftar Makanan: ");
-        for (int i = 0; i < makan.size(); i++) {
-            System.out.println((i + 1) + ". " + makan.get(i));
-        }
         System.out.println("");
         do {
             System.out.println("1. Tambah makanan");
@@ -331,6 +334,14 @@ public class CariKalori {
             System.out.print("Nama makanan: ");
             tambah = scan.next() + scan.nextLine();
             makan.add(tambah);
+            System.out.print("Berapa kalori: ");
+            while (!scan.hasNextInt()) {
+                System.out.println("Input tidak valid !!");
+                scan.next();
+                System.out.print("Berapa kalori: ");
+            }
+            kalori = scan.nextInt();
+            kalorimakan.add(kalori);
             System.out.println("Makanan berhasil ditambahkan !!");
             System.out.println("Apakah anda ingin menambah daftar makanan lagi ? (y/n)");
             ya = scan.next();
@@ -341,20 +352,14 @@ public class CariKalori {
             }
         } while (lagi);
         System.out.println("");
-        return makan;
     }
 
-    public ArrayList<String> editmakan(ArrayList<String> makan) {
-        int angka, pilih;
+    public ArrayList<String> editmakan(ArrayList<String> makan, ArrayList<Integer> kalorimakan) {
+        int angka, pilih, kalori;
         String makanan;
         System.out.print("Edit daftar makanan: ");
-        System.out.println("Daftar Makanan: ");
-        for (int i = 0; i < makan.size(); i++) {
-            System.out.println((i + 1) + ". " + makan.get(i));
-        }
-        System.out.println("");
         System.out.print("Edit daftar makanan nomor berapa?(pilih 0 untuk keluar): ");
-        while(!scan.hasNextInt()){
+        while (!scan.hasNextInt()) {
             System.out.println("Input tidak valid !!");
             scan.next();
             System.out.print("Edit daftar makanan no berapa?(pilih 0 untuk keluar): ");
@@ -366,27 +371,30 @@ public class CariKalori {
                 System.out.println("Nama makanan: ");
                 makanan = scan.nextLine() + scan.nextLine();
                 makan.set(angka - 1, makanan);
+                System.out.println("Berapa kalori: ");
+                while (!scan.hasNextInt()) {
+                    System.out.println("Input tidak valid !!");
+                    scan.next();
+                    System.out.print("Berapa kalori: ");
+                }
+                kalori = scan.nextInt();
+                kalorimakan.set(angka - 1, kalori);
             } else {
                 System.out.println("Tidak ada yang bisa diedit !!");
             }
-        }
-        else if (angka < 0){
+        } else if (angka < 0) {
             System.out.println("Angka tidak valid !!");
         }
         System.out.println("");
         return makan;
     }
 
-    public ArrayList<String> hapusmakan(ArrayList<String> makan) {
+    public ArrayList<String> hapusmakan(ArrayList<String> makan, ArrayList<Integer> kalorimakan) {
         int angka, pilih;
         System.out.print("Hapus daftar makanan = ");
-        System.out.println("Daftar Makanan: ");
-        for (int i = 0; i < makan.size(); i++) {
-            System.out.println((i + 1) + ". " + makan.get(i));
-        }
         System.out.println("");
         System.out.print("Hapus daftar makanan no berapa?(pilih 0 untuk keluar): ");
-        while(!scan.hasNextInt()){
+        while (!scan.hasNextInt()) {
             System.out.println("Input tidak valid !!");
             scan.next();
             System.out.print("Hapus daftar makanan no berapa?(pilih 0 untuk keluar): ");
@@ -397,9 +405,9 @@ public class CariKalori {
                 System.out.println("Tidak ada yang bisa dihapus !!");
             } else {
                 makan.remove(angka - 1);
+                kalorimakan.remove(angka - 1);
             }
-        }
-        else if (angka < 0){
+        } else if (angka < 0) {
             System.out.println("Angka tidak valid !!");
         }
         System.out.println("");
@@ -409,14 +417,73 @@ public class CariKalori {
     public int targetkalori() {
         int target;
         System.out.print("Target kalori yang ingin dicapai : ");
-        while(!scan.hasNextInt()){
+        while (!scan.hasNextInt()) {
             System.out.println("Input tidak valid !!");
             scan.next();
             System.out.print("Target kalori yang ingin dicapai : ");
         }
         target = scan.nextInt();
-        
+
         return target;
+    }
+
+    public void simulasi(int kalori, ArrayList<Integer> kaloriolahraga, ArrayList<Integer> kalorimakan) {
+        int makankali, olahragakali, pilihmakan, totalkalorimakan,piliholahraga,totalkaloriolahraga,total;
+        totalkalorimakan = 0;
+        totalkaloriolahraga = 0;
+        System.out.println("");
+        System.out.println("Target Kalori = " + kalori);
+        System.out.print("Makan berapa kali ? ");
+        while (!scan.hasNextInt()) {
+            System.out.println("Input tidak valid !!");
+            scan.next();
+            System.out.print("Makan berapa kali ? ");
+        }
+        makankali = scan.nextInt();
+        for (int i = 0; i < makankali; i++) {
+            do{
+            System.out.print("pilih makanan ke-" + (i+1) + "(masukkan nomor makanan pilihan) : ");
+            while (!scan.hasNextInt()) {
+                System.out.println("Input tidak valid !!");
+                scan.next();
+                System.out.print("pilih makanan ke-" + (i+1) + "(masukkan nomor makanan pilihan) : ");
+            }
+            pilihmakan = scan.nextInt();
+            if ((kalorimakan.size() < pilihmakan) && (pilihmakan<0) ){
+                System.out.println("Input tidak valid !!");
+            }
+            }while((kalorimakan.size() < pilihmakan) && (pilihmakan<0));
+            totalkalorimakan = totalkalorimakan + kalorimakan.get(pilihmakan-1);
+        }
+        System.out.print("Olahraga berapa kali ? ");
+        while (!scan.hasNextInt()) {
+            System.out.println("Input tidak valid !!");
+            scan.next();
+            System.out.print("Olahraga berapa kali ? ");
+        }
+        olahragakali = scan.nextInt();
+        for (int i = 0; i < olahragakali; i++) {
+            do{
+            System.out.print("pilih olahraga ke-" + (i+1) + "(masukkan nomor makanan pilihan) : ");
+            while (!scan.hasNextInt()) {
+                System.out.println("Input tidak valid !!");
+                scan.next();
+                System.out.print("pilih olahraga ke-" + (i+1) + "(masukkan nomor makanan pilihan) : ");
+            }
+            piliholahraga = scan.nextInt();
+            if (kaloriolahraga.size() < piliholahraga){
+                System.out.println("Input tidak valid !!");
+            }
+            }while(kalorimakan.size() < piliholahraga);
+            totalkaloriolahraga = totalkaloriolahraga + kaloriolahraga.get(piliholahraga-1);
+        }
+        total = totalkalorimakan - totalkaloriolahraga;
+        if (total < kalori){
+            System.out.println("Target kalori harian sudah tercapai !!");
+        }
+        else{
+            System.out.println("Target kalori harian belum tercapai !!");
+        }
     }
 
     public void keluar() {
@@ -424,10 +491,12 @@ public class CariKalori {
     }
 
     public static void main(String[] args) {
-        int pilih,kalori;
+        int pilih, kalori;
         float bmr;
         ArrayList<String> olahraga = new ArrayList<String>();
+        ArrayList<Integer> kaloriolahraga = new ArrayList<Integer>();
         ArrayList<String> makan = new ArrayList<String>();
+        ArrayList<Integer> kalorimakan = new ArrayList<Integer>();
         System.out.println("Starting the Application");
         CariKalori fungsi = new CariKalori();
         fungsi.boot();
@@ -447,35 +516,41 @@ public class CariKalori {
                     break;
                 }
                 case 3: {
-                    fungsi.daftarolahraga(olahraga);
+                    fungsi.daftarolahraga(olahraga, kaloriolahraga);
                     break;
                 }
                 case 4: {
-                    fungsi.tambaholahraga(olahraga);
+                    fungsi.daftarolahraga(olahraga, kaloriolahraga);
+                    fungsi.tambaholahraga(olahraga, kaloriolahraga);
                     break;
                 }
                 case 5: {
-                    fungsi.editolahraga(olahraga);
+                    fungsi.daftarolahraga(olahraga, kaloriolahraga);
+                    fungsi.editolahraga(olahraga, kaloriolahraga);
                     break;
                 }
                 case 6: {
-                    fungsi.hapusolahraga(olahraga);
+                    fungsi.daftarolahraga(olahraga, kaloriolahraga);
+                    fungsi.hapusolahraga(olahraga, kaloriolahraga);
                     break;
                 }
                 case 7: {
-                    fungsi.daftarmakan(makan);
+                    fungsi.daftarmakan(makan, kalorimakan);
                     break;
                 }
                 case 8: {
-                    fungsi.tambahmakan(makan);
+                    fungsi.daftarmakan(makan, kalorimakan);
+                    fungsi.tambahmakan(makan, kalorimakan);
                     break;
                 }
                 case 9: {
-                    fungsi.editmakan(makan);
+                    fungsi.daftarmakan(makan, kalorimakan);
+                    fungsi.editmakan(makan, kalorimakan);
                     break;
                 }
                 case 10: {
-                    fungsi.hapusmakan(makan);
+                    fungsi.daftarmakan(makan, kalorimakan);
+                    fungsi.hapusmakan(makan, kalorimakan);
                     break;
                 }
                 case 11: {
@@ -483,6 +558,11 @@ public class CariKalori {
                     break;
                 }
                 case 12: {
+                    fungsi.daftarolahraga(olahraga, kaloriolahraga);
+                    fungsi.daftarmakan(makan, kalorimakan);
+                    fungsi.simulasi(kalori, kaloriolahraga, kalorimakan);
+                }
+                case 13: {
                     fungsi.keluar();
                     break;
                 }
@@ -491,7 +571,7 @@ public class CariKalori {
                 }
             }
 
-        } while (pilih != 12);
+        } while (pilih != 13);
 
     }
 }
